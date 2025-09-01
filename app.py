cee28fe75b9bdf3a706641887d2d10f09ad88fa0
MohitSharma2024 <mohitsharma.22@nshm.edu.in>
2025-09-01 18:42:49 +0530

    url routing

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 
-#creating Flask object 
-#also telling flask how the app is ran 
+#creating Flask object + also telling flask how the app is ran
 app = Flask(__name__)
-#if ran directly n
- 
+
+#url Routing
+
+
+@app.route("/") #default method = ["GET"]
+def welcome():
+    
+    return "<h1>hello</h1>"
+
+@app.route("/index")
+def index():
+    return "welcome to index page"
+
+#if ran directly n 
 if __name__ == "__main__" :
     #start server 
     app.run(debug = True) # url: localhost , port: 500
